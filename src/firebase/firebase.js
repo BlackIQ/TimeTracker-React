a81f7f286b278038f1e4fe7ccd53c71623ca2c20
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-29 10:15:22 +0430

    Added firebase methods

---
+++ src/firebase/firebase.js
@@ -0,0 +1,96 @@
+import {initializeApp} from "firebase/app";
+
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    sendPasswordResetEmail,
+    GoogleAuthProvider,
+    signInAnonymously,
+    signInWithPopup,
+    getAuth,
+    signOut,
+} from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDdpxQkEbrTM5tGKZ7GuaMT6qziUt0J-Og",
+    authDomain: "reactjs-time-tracker.firebaseapp.com",
+    projectId: "reactjs-time-tracker",
+    storageBucket: "reactjs-time-tracker.appspot.com",
+    messagingSenderId: "1083271030583",
+    appId: "1:1083271030583:web:fc585478f7292469145cb2",
+    measurementId: "G-XB7L1RMH2Q"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+// Google Authentication
+const googleAuth = async () => {
+    try {
+        await signInWithPopup(auth, googleProvider);
+    } catch (error) {
+        console.error(error);
+        alert(error.message);
+    }
+}
+
+const anonAuth = async () => {
+    try {
+        await signInAnonymously(auth);
+    } catch (error) {
+        console.error(error);
+        alert(error.message);
+    }
+}
+
+// Email and Password Authentication
+const emailPasswordAuth = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.error(error);
+        alert(error.message);
+    }
+}
+
+// Register a new user
+const register = async (email, password) => {
+    try {
+        await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.error(error);
+        alert(error.message);
+    }
+}
+
+// Logout
+const logout = () => {
+    try {
+        signOut(auth);
+    } catch (error) {
+        console.error(error);
+        alert(error.message);
+    }
+}
+
+const reset = async (email) => {
+    try {
+        await sendPasswordResetEmail(auth, email);
+        alert('Email sent. Check you inbox.')
+    } catch (error) {
+        console.error(error);
+        alert(error.message);
+    }
+}
+
+export {
+    emailPasswordAuth,
+    googleAuth,
+    register,
+    anonAuth,
+    logout,
+    reset,
+    auth,
+}
