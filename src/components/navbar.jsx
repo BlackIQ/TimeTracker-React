f7e0fb5962a86f57f79411876e08412004abd505
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-29 10:21:10 +0430

    Added a sample nav changer link for auth

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,25 @@
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {auth} from "../firebase/firebase";
+import {useEffect, useState} from "react";
 
 const Navbar = () => {
+
+    const [navLink, setNavLink] = useState('');
+
+    const [user, loading, error] = useAuthState(auth);
+
+    useEffect(() => {
+        if (loading) return;
+        if (user) setNavLink(<Link>Logout</Link>)
+        else setNavLink(<Link>Auth</Link>)
+    }, [user, loading])
+
     return (
         <nav className='navbar navbar-light bg-light'>
             <div className='container'>
                 <Link className='navbar-brand' to='/'>Time Tracker</Link>
+                {navLink}
             </div>
         </nav>
     );
