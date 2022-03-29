9dcfff4209fb0c06e47b7dbb1a4517f4f598e311
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-29 10:21:29 +0430

    Added auth checker for home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,19 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {useHistory} from "react-router-dom";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {auth} from "../firebase/firebase";
 
 const Home = () => {
+
+    const history = useHistory();
+
+    const [user, loading, error] = useAuthState(auth);
+
+    useEffect(() => {
+        if (loading) return;
+        if (!user) history.push('/auth');
+    }, [user, loading])
+
     const [tracks, setTracks] = useState([
         {name: 'Amir', id: 1},
     ]);
