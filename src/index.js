6b2d9c2b1914e7676e7e1d38b40918a8c8798ca1
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-29 09:54:31 +0430

    Import mdb js and css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'mdb-ui-kit/css/mdb.min.css';
+import 'mdb-ui-kit/js/mdb.min.js';
 
 ReactDOM.render(
   <App />, document.getElementById('root')
