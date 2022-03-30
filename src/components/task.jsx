434a934d8eb289625893920c3562a53d675e00c4
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-30 12:45:29 +0430

    Handle delete

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,10 +1,25 @@
+import {doc, deleteDoc} from "firebase/firestore";
+import {db} from "../firebase/firebase";
+
 const Task = (props) => {
     const task = props.task;
+
+    const handleDelete = async () => {
+        try {
+            const t = doc(db, 'tasks', task.id);
+            await deleteDoc(t);
+        } catch (error) {
+            console.error(error);
+            alert(error.message);
+        }
+    }
 
     return (
         <div key={task.id}>
             <div className='border border-primary rounded-4 p-3'>
                 <span>{task.name}</span>
+                <hr className='text-primary'/>
+                <span className={'pointer text-danger'} onClick={() => handleDelete()}>Delete</span>
             </div>
             <br/>
         </div>
