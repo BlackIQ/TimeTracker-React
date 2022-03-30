fc210056bf3c43a8ded65bb25ac4abe385796f58
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-30 12:59:56 +0430

    Added task

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,4 +1,4 @@
-import {doc, deleteDoc} from "firebase/firestore";
+import {doc, deleteDoc, updateDoc} from "firebase/firestore";
 import {db} from "../firebase/firebase";
 
 const Task = (props) => {
@@ -14,10 +14,26 @@ const Task = (props) => {
         }
     }
 
+    const handleUpdate = async () => {
+        try {
+            const data = {
+                'status': !task.status,
+            };
+            const t = doc(db, 'tasks', task.id);
+            await updateDoc(t, data);
+        } catch (error) {
+            console.error(error);
+            alert(error.message);
+        }
+    }
+
     return (
         <div key={task.id}>
             <div className='border border-primary rounded-4 p-3'>
-                <span>{task.name}</span>
+                <div className='pointer' onClick={() => handleUpdate()}>
+                    <input type='checkbox' className='form-check-input' checked={task.status}/>
+                    <span>{task.name}</span>
+                </div>
                 <hr className='text-primary'/>
                 <span className={'pointer text-danger'} onClick={() => handleDelete()}>Delete</span>
             </div>
