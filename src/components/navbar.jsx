b56e31f74b7e19bc7ef9399bc3badfd6bf157fb6
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-04-03 20:27:01 +0430

    Added dropdown

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,22 +4,30 @@ import {auth, logout} from "../firebase/firebase";
 import {useEffect, useState} from "react";
 
 const Navbar = () => {
-
-    const [navLink, setNavLink] = useState('');
-
     const [user, loading, error] = useAuthState(auth);
 
     useEffect(() => {
         if (loading) return;
-        if (user) setNavLink(<Link className='text-white' onClick={() => logout()}>Logout</Link>)
-        else setNavLink(<Link className='text-white'>Auth</Link>)
     }, [user, loading])
 
     return (
         <nav className='navbar navbar-light bg-primary shadow-0'>
             <div className='container'>
                 <Link className='navbar-brand text-white' to='/'>Time Tracker</Link>
-                {navLink}
+                {
+                    user ? <div className='btn-group shadow-0'>
+                        <span className="text-white pointer" data-mdb-toggle="dropdown">Options</span>
+                        <ul className="dropdown-menu">
+                            <li><span className="dropdown-item pointer text-primary">All</span></li>
+                            <li><span className="dropdown-item pointer text-success">Done</span></li>
+                            <li><span className="dropdown-item pointer text-danger">Not done</span></li>
+                            <li>
+                                <hr className="dropdown-divider"/>
+                            </li>
+                            <li><span className="dropdown-item pointer" onClick={() => logout()}>Logout</span></li>
+                        </ul>
+                    </div> : <Link className='text-white'>Auth</Link>
+                }
             </div>
         </nav>
     );
