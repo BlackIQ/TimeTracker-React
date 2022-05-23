93b4002f6bc4907310215e2e73c04177dfed4ed0
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-23 12:29:23 +0430

    Fixed navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,34 +1,25 @@
-import {Link, useHistory} from "react-router-dom";
+import {Link} from "react-router-dom";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {auth, logout} from "../firebase/firebase";
 import {useEffect, useState} from "react";
 
 const Navbar = () => {
-    const [user, loading, error] = useAuthState(auth);
 
+    const [navLink, setNavLink] = useState('');
+
+    const [user, loading, error] = useAuthState(auth);
 
     useEffect(() => {
         if (loading) return;
+        if (user) setNavLink(<Link className='text-white' onClick={() => logout()}>Logout</Link>)
+        else setNavLink(<Link className='text-white'>Auth</Link>)
     }, [user, loading])
 
     return (
         <nav className='navbar navbar-light bg-primary shadow-0'>
             <div className='container'>
                 <Link className='navbar-brand text-white' to='/'>Time Tracker</Link>
-                {
-                    user ? <div className='btn-group shadow-0'>
-                        <span className="text-white pointer" data-mdb-toggle="dropdown">Options</span>
-                        <ul className="dropdown-menu">
-                            <li><Link className="dropdown-item pointer text-primary" to={'/'}>All</Link></li>
-                            <li><Link className="dropdown-item pointer text-success" to={'/done'}>Done</Link></li>
-                            <li><Link className="dropdown-item pointer text-danger" to={'/not'}>Not done</Link></li>
-                            <li>
-                                <hr className="dropdown-divider"/>
-                            </li>
-                            <li><span className="dropdown-item pointer" onClick={() => logout()}>Logout</span></li>
-                        </ul>
-                    </div> : <Link className='text-white'>Auth</Link>
-                }
+                {navLink}
             </div>
         </nav>
     );
